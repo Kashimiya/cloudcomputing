1f87aabf4acfda9485d372c260c58e03e786c1e4
iris <429328398@qq.com>
2020-11-29 10:03:03 +0800

    modify the wordcloud part and add the manage of data of tags

--- wordcloudtest/presentation.py
+++ wordcloudtest/presentation.py
@@ -2,64 +2,32 @@
 import matplotlib.pyplot as plt
 import jieba
 from wordcloud import WordCloud
+import json
 
-path_from="D:/大三上/云计算/cloud_data/name/"
+path_from="D:/大三上/云计算/data/"
 path_to="D:/大三上/云计算/爬虫/wordcloudtest/pic/"
 
-def getFileList():
-    import os
-    file_list = os.listdir(path_from)
-    return file_list
-# 1.读入txt文本数据
-for filename in getFileList():
-        print(filename)
-        f1 = open(path_from + filename, 'rb')
-        text = f1.read().decode("utf-8")
-        text=str(text)
-        #print(text)
-        # 2.分词
-        temp = text.split('\n')
-        temp3=[]#姓氏
-        temp4=[]#名
-        flag=True
-        for item in temp:
-                temp1=item.split(';')
-                for item1 in temp1:
-                        # print(flag)
-                        if len(item1)==0:
-                                break
-                        if item1=="None" or item1=="one":
-                                continue
-                        if flag:
-                                if item1[0]=='[':
-                                        temp2=item1[2:-2]
-                                        # print(temp2)
-                                        if temp2!='':
-                                                temp3.extend(temp2.split(("', '")))
-                                        flag=False
-                        else:
-                                if item1[0]=='[':
-                                        temp2=item1[2:-3]
-                                        # print(temp2)
-                                        if temp2 != '':
-                                                temp4.extend(temp2.split(("', '")))
-                                        flag=True
-        # print(type(cut_text))
-        # 必须给个符号分隔开分词结果来形成字符串,否则不能绘制词云
-        ans1={}
-        for item in temp3:
-                if ans1.keys().__contains__(item):
-                        ans1[item]=ans1[item]+1
-                else:
-                        ans1[item]=1
-        ans2={}
-        for item in temp4:
-                if ans2.keys().__contains__(item):
-                        ans2[item]=ans2[item]+1
-                else:
-                        ans2[item]=1
-        # 3.生成词云图，这里需要注意的是WordCloud默认不支持中文，所以这里需已下载好的中文字库
-        # 无自定义背景图：需要指定生成词云图的像素大小，默认背景颜色为黑色,统一文字颜色：mode='RGBA'和colormap='pink'
+text_firstname=open(path_from+"date_firstname.json",'rb').read().decode("utf-8")
+text_firstname=json.loads(text_firstname)
+text_lastname=open(path_from+"times_lastname.json",'rb').read().decode("utf-8")
+text_lastname=json.loads(text_lastname)
+ans1={}
+ans2={}
+for key in text_firstname:
+        temp=key[1:-1].split(',')
+        if temp[0] in ans1:
+                ans1[temp[0]][temp[1]]=text_firstname[key]
+        else:
+                ans1[temp[0]]={temp[1]:text_firstname[key]}
+for key in text_lastname:
+        temp = key[1:-1].split(',')
+        if temp[0] in ans2:
+                ans2[temp[0]][temp[1]] = text_lastname[key]
+        else:
+                ans2[temp[0]] = {temp[1]: text_lastname[key]}
+# 3.生成词云图，这里需要注意的是WordCloud默认不支持中文，所以这里需已下载好的中文字库
+# 无自定义背景图：需要指定生成词云图的像素大小，默认背景颜色为黑色,统一文字颜色：mode='RGBA'和colormap='pink'
+for item in ans1:
         wc = WordCloud(
                 # 设置字体，不指定就会出现乱码
                 # 设置背景色
@@ -77,8 +45,27 @@ for filename in getFileList():
                 #colormap='pink'
                 )
         # 产生词云
-        wc.generate_from_frequencies(ans1)
+        wc.generate_from_frequencies(ans1[item])
         # 保存图片
-        wc.to_file(path_to+filename[:-4]+"姓.png") # 按照设置的像素宽高度保存绘制好的词云图，比下面程序显示更清晰
-        wc.generate_from_frequencies(ans2)
-        wc.to_file(path_to+filename[:-4]+"名.png") # 按照设置的像素宽高度保存绘制好的词云图，比下面程序显示更清晰
+        wc.to_file(path_to+item+"名.png") # 按照设置的像素宽高度保存绘制好的词云图，比下面程序显示更清晰
+for item in ans2:
+        wc = WordCloud(
+                # 设置字体，不指定就会出现乱码
+                # 设置背景色
+                font_path="STKAITI.TTF",
+                background_color='white',
+                # 设置背景宽
+                width=500,
+                # 设置背景高
+                height=350,
+                # 最大字体
+                max_font_size=50,
+                # 最小字体
+                min_font_size=10,
+                mode='RGBA'
+                # colormap='pink'
+        )
+        # 产生词云
+        wc.generate_from_frequencies(ans2[item])
+        # 保存图片
+        wc.to_file(path_to + item + "姓.png")  # 按照设置的像素宽高度保存绘制好的词云图，比下面程序显示更清晰
